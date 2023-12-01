032c73c6e0b9773f691ea729adb32c73fc6b983e
jonatasbreglescam <jonatasbreglescam@gmail.com>
2023-12-01 14:59:02 -0300

    css carrinho

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('snkrhead02', 'root', 'root', {
+const sequelize = new Sequelize('snkrhead', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
 });
